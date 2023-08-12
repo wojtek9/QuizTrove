2c22f705283cd021c09312a44895fd4c88632aee
wojtek9 <donut_qewbs@hotmail.com>
2023-08-12 17:09:13 +0200

    login update

--- src/components/LoginView.tsx
+++ src/components/LoginView.tsx
@@ -1,80 +1,50 @@
 import React from "react";
 import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
   MDBBtn,
-  MDBIcon,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
   MDBInput,
-  MDBCheckbox,
-  MDBAccordion,
+  MDBRow,
+  MDBCol,
 } from "mdb-react-ui-kit";
 
 function Login() {
   return (
-    <MDBContainer fluid className="p-3 my-5">
-      <MDBRow className="page-header">
-        <div>
-          <h1>Language Games</h1>
-        </div>
-      </MDBRow>
-      <MDBRow>
-        <MDBCol col="10" md="6">
-          <img src="/loginimage.jpg" className="img-fluid" alt="image" />
-        </MDBCol>
-        <MDBCol col="4" md="5">
-          <MDBInput
-            className="logininput"
-            wrapperClass="mb-4"
-            label="Email address"
-            id="formControlLg"
-            type="email"
-            size="lg"
-          />
-          <MDBInput
-            className="logininput"
-            wrapperClass="mb-4"
-            label="Password"
-            id="formControlLg"
-            type="password"
-            size="lg"
-          />
-
-          <div className="d-flex justify-content-between mx-4 mb-4">
-            <MDBCheckbox
-              name="flexCheck"
-              value=""
-              id="flexCheckDefault"
-              label="Remember me"
-            />
-            <a href="!#">Forgot password?</a>
-          </div>
-
-          <MDBBtn className="mb-4 w-100" size="lg">
-            Sign in
-          </MDBBtn>
-
-          <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0">OR</p>
-          </div>
-
-          <MDBBtn
-            className="mb-4 w-100"
-            size="lg"
-            style={{ backgroundColor: "#3b5998" }}
+    <MDBContainer className="my-5" style={{ width: "800px" }}>
+      <MDBRow className="g-0 align-items-center">
+        <MDBCol col="6">
+          <MDBCard
+            className="my-5 cascading-right"
+            style={{
+              background: "hsla(0, 0%, 100%, 0.55)",
+              backdropFilter: "blur(30px)",
+            }}
           >
-            <MDBIcon fab icon="facebook-f" className="mx-2" />
-            Continue with facebook
-          </MDBBtn>
+            <MDBCardBody className="p-5 shadow-5 text-center">
+              <h2 className="fw-bold mb-5">Sign in</h2>
 
-          <MDBBtn
-            className="mb-4 w-100"
-            size="lg"
-            style={{ backgroundColor: "#55acee" }}
-          >
-            <MDBIcon fab icon="twitter" className="mx-2" />
-            Continue with twitter
-          </MDBBtn>
+              <MDBInput
+                className="w-50 mx-auto"
+                wrapperClass="mb-4"
+                label="Email"
+                id="form3"
+                type="email"
+              />
+              <MDBInput
+                className="w-50 mx-auto"
+                wrapperClass="mb-4"
+                label="Password"
+                id="form4"
+                type="password"
+              />
+
+              <MDBBtn className="mb-4">Login</MDBBtn>
+              <div>
+                <a href="!#">Forgot password?</a>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
